2f104882cdacb5a7ce335ba2304b4ee4cbe2c9f8
Thien Nhan Tran <127055768+JaydenT107@users.noreply.github.com>
2025-01-19 18:51:34 -0600

    Update Streamlit_script.py

--- Streamlit_script.py
+++ Streamlit_script.py
@@ -1,9 +1,12 @@
 import boto3
 import pandas as pd
 from io import StringIO
-
+import streamlit as st
+import os
 def get_date():
-  s3 = boto3.client('s3')
+  aws_access_key = os.getenv("AWS_ACCESS_KEY_ID")
+  aws_secret_key = os.getenv("AWS_SECRET_ACCESS_KEY")
+  s3 = boto3.client('s3', aws_access_key_id=aws_access_key, aws_secret_access_key=aws_secret_key)
   
   response = s3.get_object(Bucket = 'stocksectordata' , Key = 'sector_list.txt')
   sector_string = response['Body'].read().decode('utf-8')
