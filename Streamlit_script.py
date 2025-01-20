8b47b120c00aaaf1ffd3e1281b401bb1c54642a8
Thien Nhan Tran <127055768+JaydenT107@users.noreply.github.com>
2025-01-19 19:33:37 -0600

    Update Streamlit_script.py

--- Streamlit_script.py
+++ Streamlit_script.py
@@ -29,7 +29,7 @@ aws_secret_key = os.getenv("AWS_SECRET_ACCESS_KEY")
 
 if not aws_access_key or not aws_secret_key:
     st.error("AWS credentials are missing. Please set AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY.")
-    return
+    
 
 
 
