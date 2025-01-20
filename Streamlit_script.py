23fb171459c7d4d29afbc3da2885e82b83adfab5
Thien Nhan Tran <127055768+JaydenT107@users.noreply.github.com>
2025-01-19 19:18:28 -0600

    Update Streamlit_script.py

--- Streamlit_script.py
+++ Streamlit_script.py
@@ -9,7 +9,7 @@ st.set_page_config(page_title="Top 5 Stock", layout="wide")
 def get_data():
   aws_access_key = os.getenv("AWS_ACCESS_KEY_ID")
   aws_secret_key = os.getenv("AWS_SECRET_ACCESS_KEY")
-  s3 = boto3.client('s3', aws_access_key_id=aws_access_key, aws_secret_access_key=aws_secret_key)
+  s3 = boto3.client('s3', aws_access_key_id=aws_access_key, aws_secret_access_key=aws_secret_key, region_name = 'us-east-2')
   
   response = s3.get_object(Bucket = 'stocksectordata' , Key = 'sector_list.txt')
   sector_string = response['Body'].read().decode('utf-8')
