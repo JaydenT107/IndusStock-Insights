06ae9c2ddded27b55d954a5f32b78e07dc6a6649
Thien Nhan Tran <127055768+JaydenT107@users.noreply.github.com>
2025-01-19 20:03:55 -0600

    Update Streamlit_script.py

--- Streamlit_script.py
+++ Streamlit_script.py
@@ -7,9 +7,8 @@ import os
 st.set_page_config(page_title="Top 5 Stocks", layout="wide")
 
 def get_data():
-  aws_access_key = os.getenv("AWS_ACCESS_KEY_ID")
-  aws_secret_key = os.getenv("AWS_SECRET_ACCESS_KEY")
-  s3 = boto3.client('s3', aws_access_key_id=aws_access_key, aws_secret_access_key=aws_secret_key, region_name = 'us-east-2')
+
+  s3 = boto3.client('s3')
   
   response = s3.get_object(Bucket = 'stocksectordata' , Key = 'sector_list.txt')
   sector_string = response['Body'].read().decode('utf-8')
@@ -24,14 +23,7 @@ def get_data():
   return df
 
 
-aws_access_key = os.getenv("AWS_ACCESS_KEY_ID")
-aws_secret_key = os.getenv("AWS_SECRET_ACCESS_KEY")
-
-if not aws_access_key or not aws_secret_key:
-    st.error("AWS credentials are missing. Please set AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY!")
-else:
-    st.write("AWS credentials found.")
-    
+st.dataframe(get_data())
 
 
 
