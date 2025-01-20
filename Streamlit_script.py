3fab5677d7156e6fcc2c1d6534ef5946cc30139b
Thien Nhan Tran <127055768+JaydenT107@users.noreply.github.com>
2025-01-20 13:28:26 -0600

    Update Streamlit_script.py

--- Streamlit_script.py
+++ Streamlit_script.py
@@ -1,33 +1,26 @@
-import boto3
-import pandas as pd
-from io import StringIO
 import streamlit as st
-import os
-
-st.set_page_config(page_title="Top 5 Stocks", layout="wide")
+import boto3
+from io import StringIO
+import pandas as pd
 
 def get_data():
+    s3 = boto3.client(
+        's3',
+        aws_access_key_id=st.secrets["AWS_ACCESS_KEY_ID"],
+        aws_secret_access_key=st.secrets["AWS_SECRET_ACCESS_KEY"],
+        region_name=st.secrets.get("AWS_DEFAULT_REGION", "us-east-2")
+    )
 
-  s3 = boto3.client('s3')
-  
-  response = s3.get_object(Bucket = 'stocksectordata' , Key = 'sector_list.txt')
-  sector_string = response['Body'].read().decode('utf-8')
-  
-  sector_list = sector_string.split(', ')
-  print(sector_list)
-  
-  data_response = s3.get_object(Bucket = 'stocksectordata', Key = f'{sector_list[0]}/Data/stock_1.csv')
-  data = data_response['Body'].read().decode('utf-8')
-  
-  df = pd.read_csv(StringIO(data))
-  return df.head(2)
+    response = s3.get_object(Bucket='stocksectordata', Key='sector_list.txt')
+    sector_string = response['Body'].read().decode('utf-8')
 
+    sector_list = sector_string.split(', ')
+    st.write(sector_list)
+
+    data_response = s3.get_object(Bucket='stocksectordata', Key=f'{sector_list[0]}/Data/stock_1.csv')
+    data = data_response['Body'].read().decode('utf-8')
+
+    df = pd.read_csv(StringIO(data))
+    return df.head(2)
 
 st.write(get_data())
-
-
-
-
-
-
-
