9ef09853ee92b8879d0f1f6c7f70a39ba317fd9f
Thien Nhan Tran <127055768+JaydenT107@users.noreply.github.com>
2025-01-19 19:12:53 -0600

    Update Streamlit_script.py

--- Streamlit_script.py
+++ Streamlit_script.py
@@ -6,7 +6,7 @@ import os
 
 st.set_page_config(page_title="Top 5 Stock", layout="wide")
 
-def get_date():
+def get_data():
   aws_access_key = os.getenv("AWS_ACCESS_KEY_ID")
   aws_secret_key = os.getenv("AWS_SECRET_ACCESS_KEY")
   s3 = boto3.client('s3', aws_access_key_id=aws_access_key, aws_secret_access_key=aws_secret_key)
