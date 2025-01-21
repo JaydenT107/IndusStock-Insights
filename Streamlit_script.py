2e46864e5c370a41fa53224b4e62744688f1eb08
Thien Nhan Tran <127055768+JaydenT107@users.noreply.github.com>
2025-01-20 22:45:53 -0600

    a

--- Streamlit_script.py
+++ Streamlit_script.py
@@ -15,14 +15,18 @@ def get_data():
     response = s3.get_object(Bucket='stocksectordata', Key='sector_list.txt')
     sector_string = response['Body'].read().decode('utf-8')
 
+    name_response = s3.get_object(Bucket='stocksectordata', Key=f'{sector_list[0]}/Name/stock_{num}.csv')
+    name = name_response['Body'].read().decode('utf-8')
+
     sector_list = sector_string.split(', ')
     tables = []
     for num in range(1,6):
         data_response = s3.get_object(Bucket='stocksectordata', Key=f'{sector_list[0]}/Data/stock_{num}.csv')
         data = data_response['Body'].read().decode('utf-8')
+       
         df = pd.read_csv(StringIO(data))
         tables.append(df)
-    return tables
+    return [tables,name]
 
 def check_color(data):
     if data.iloc[0]['Close'] > data.iloc[-1]['Close']:
@@ -32,21 +36,26 @@ def check_color(data):
     elif data.iloc[0]['Close'] == data.iloc[-1]['Close']:
         return '#FFFF00'
     
-def line_chart(data):
+def line_chart(data,name):
     close_min = data['Close'].min()
     close_max = data['Close'].max()
     fig = px.line(data,x = 'Date' , y = 'Close')
     fig.update_yaxes(range=[close_min,close_max])
     fig.update_traces(line = dict(color = check_color(data) ))
     fig.update_xaxes(nticks = 5)
+    fig.update_layout(
+    title=name,  
+    title_x=0.5,            
+    title_font=dict(size=24, family='Arial', color='blue') 
+)
     
 
     return st.plotly_chart(fig, use_container_width = True)
 
 
 def generate_chart():
-    data = get_data()
+    data,name = get_data()
     for i in range(0,5):
-        line_chart(data[i].head(30))
+        line_chart(data[i].head(30),name[i])
 
 generate_chart()
