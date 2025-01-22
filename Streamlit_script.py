ee903446cee93de0e02509011ccdfd411588930e
Thien Nhan Tran <127055768+JaydenT107@users.noreply.github.com>
2025-01-22 01:02:38 -0600

    a

--- Streamlit_script.py
+++ Streamlit_script.py
@@ -7,6 +7,17 @@ from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
 st.set_page_config(layout="wide")
+st.markdown("""
+    <style>
+        .stSelectbox>div>div>div>div>div {
+            background-color: black;
+            color: white;
+        }
+        .stSelectbox>div>div>div>div>div>input {
+            color: white;
+        }
+    </style>
+""", unsafe_allow_html=True)
 
 def date_selectbox():
     end_date = datetime.now()
