39c81107bf4b5e7f1f10b2295801f8615ac4f57f
Thien Nhan Tran <127055768+JaydenT107@users.noreply.github.com>
2025-01-23 01:30:12 -0600

    a

--- Streamlit_script.py
+++ Streamlit_script.py
@@ -124,7 +124,7 @@ def generate_chart():
 
     with col1:
         st.markdown(f"<h1 style='font-size: 60px; color: white;'>{sector}</h1>", unsafe_allow_html=True)
-        st.markdown(f"{new_string}")
+        t.markdown(f"<div class='fixed-height'>{AI_description}</div>"
         st.write(" ")
         line_chart(data[3],name[3],sday,eday)
 
