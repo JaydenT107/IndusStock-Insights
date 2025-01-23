ecb467dcd0f6c6f5ab4745ce485a5cdfb5b5497b
Thien Nhan Tran <127055768+JaydenT107@users.noreply.github.com>
2025-01-23 01:38:28 -0600

    a

--- Streamlit_script.py
+++ Streamlit_script.py
@@ -8,9 +8,8 @@ from dateutil.relativedelta import relativedelta
 
 st.set_page_config(layout="wide")
 st.header('**IndusStock Insight**')
-st.write("""IndusStock Insights is an AI-powered platform that helps investors make informed decisions by identifying the top 5 stocks in a specific industry.
- It pulls real-time data through a stock API and uses advanced machine learning to analyze market trends and company performance. The platform generates interactive charts and provides intelligent recommendations, guiding users on whether to buy, hold, or avoid stocks based on data-driven insights. With its combination of real-time analysis and AI forecasts, 
-IndusStock Insights empowers users to navigate the stock market confidently and optimize their investments.""")
+st.write("""IndusStock Insights is an AI-powered platform that helps investors make informed decisions by identifying the top 5 stocks in a specific industry. It pulls real-time data through a stock API and uses advanced machine learning to analyze market trends and company performance.
+The platform generates interactive charts and provides intelligent recommendations, guiding users on whether to buy, hold, or avoid stocks based on data-driven insights. With its combination of real-time analysis and AI forecasts, IndusStock Insights empowers users to navigate the stock market confidently and optimize their investments.""")
 
 
 def date_selectbox():
