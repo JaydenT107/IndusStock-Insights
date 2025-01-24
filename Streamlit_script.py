757a3e942f82ccf0fa91691ac8d5e43f63421e31
Thien Nhan Tran <127055768+JaydenT107@users.noreply.github.com>
2025-01-24 03:08:46 -0600

    a

--- Streamlit_script.py
+++ Streamlit_script.py
@@ -142,4 +142,4 @@ with col1:
 data[name.index(stock_name)]
 st.write(data[name.index(stock_name)])
 st.write(name[name.index(stock_name)])
-st.plotly_chart(line_chart(data[name.index(name)],name = None,sday,eday, date_format, title = f'{name[name.index(stock_name)]} symbol'), use_container_width = False)
+st.plotly_chart(line_chart(data[name.index(name)],name = None,sday,eday, date_format, title = 'a'), use_container_width = False)
