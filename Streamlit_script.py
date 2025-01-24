4ca2931a9a02c06039b977ea7819c054bda68204
Thien Nhan Tran <127055768+JaydenT107@users.noreply.github.com>
2025-01-24 03:52:39 -0600

    a

--- Streamlit_script.py
+++ Streamlit_script.py
@@ -138,11 +138,9 @@ def first_part():
 def second_part():
     name,data,sday,eday,date_format  = first_part()
     st.header('Explore Stock Details')
-    col1,col2 = st.columns(2)
     stock_name = st.selectbox('Select a Stock for Detailed Analysis',name)
     st.markdown(f"<h1 style='font-size: 45px; color: white;'>{stock_name}</h1>", unsafe_allow_html=True)
-    
-    with col1:
-        st.plotly_chart(line_chart(data[name.index(stock_name)],sday = sday,eday = eday, date_format = date_format, new_title = f'Price', name = None), use_container_width = True)
+    col1,col2 = st.columns(2)
+    col1.st.plotly_chart(line_chart(data[name.index(stock_name)],sday = sday,eday = eday, date_format = date_format, new_title = f'Price', name = None), use_container_width = True)
 
 second_part()
