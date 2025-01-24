86b3ba6dedf44fb72bbaa28f499e06e20656800c
Thien Nhan Tran <127055768+JaydenT107@users.noreply.github.com>
2025-01-24 03:15:51 -0600

    a

--- Streamlit_script.py
+++ Streamlit_script.py
@@ -140,4 +140,4 @@ with col1:
     stock_name = st.selectbox('Select Stock',name)
 
 
-st.plotly_chart(line_chart(data[name.index(stock_name)],sday,eday, date_format, new_title = f'Symbol: {stock_name}', name = None), use_container_width = False)
+st.plotly_chart(line_chart(data[name.index(stock_name)],sday = sday,eday = eday, date_format = date_format, new_title = f'Symbol: {stock_name}', name = None), use_container_width = False)
