b2c614633afc79cee4bec7f74f7f1dba40b241be
Thien Nhan Tran <127055768+JaydenT107@users.noreply.github.com>
2025-01-25 00:12:21 -0600

    a

--- Streamlit_script.py
+++ Streamlit_script.py
@@ -116,7 +116,7 @@ def line_chart(data,name,sday,eday,date_format,new_title=None):
 def scatter_plot(data,name,sday,eday,date_format,new_title=None):
     data['Date'] = pd.to_datetime(data['Date'], format='%m/%d/%Y')
     filtered_data = data[(data['Date'] >= sday) & (data['Date'] <= eday)]
-    fig = px.scatter(filtered_data, x = 'Volume', y = 'Close')
+    fig = px.scatter(filtered_data, x = 'Volume', y = 'Close', hover_data = ['Date'])
     return fig
 
 
