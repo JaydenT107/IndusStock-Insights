0420a8a1147cd4ea52f86344d397d423307fa2cb
Thien Nhan Tran <127055768+JaydenT107@users.noreply.github.com>
2025-01-24 21:07:35 -0600

    a

--- Streamlit_script.py
+++ Streamlit_script.py
@@ -6,10 +6,10 @@ import plotly.express as px
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
-
+st.set_page_config(layout="wide", page_title = "IndusStock Insight")
 
 def date_selectbox():
-    st.set_page_config(layout="wide", page_title = "IndusStock Insight")
+    
     st.header('**IndusStock Insight**')
     st.write("""IndusStock Insights is an AI-powered platform that helps investors make informed decisions by identifying the top 5 stocks in a specific industry. It pulls real-time data through a stock API and uses advanced machine learning to analyze market trends and company performance.
     The platform generates interactive charts and provides intelligent recommendations, guiding users on whether to buy, hold, or avoid stocks based on data-driven insights. With its combination of real-time analysis and AI forecasts, IndusStock Insights empowers users to navigate the stock market confidently and optimize their investments.""")
