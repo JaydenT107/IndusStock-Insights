2e12052abb7ef25bc927eacf9afff60493c3d038
Thien Nhan Tran <127055768+JaydenT107@users.noreply.github.com>
2025-01-25 19:42:13 -0600

    a

--- Streamlit_script.py
+++ Streamlit_script.py
@@ -150,8 +150,8 @@ def scatter_plot(data,name,sday,eday,date_format,new_title=None):
     avg_line_color = check_average(average_period,average)
 
     fig = px.scatter(filtered_data, x = 'Close', y = 'Volume', hover_data = ['Date'], color = 'Highlight', color_discrete_map={relative_title: 'red', 'Older': 'light blue'})
-    fig.add_hline(y = average, line_dash = 'solid', line_color = 'yellow', annotation_text = 'Average' , showlegend = True)
-    fig.add_hline(y = average_period, line_dash = 'dash', line_color = avg_line_color, annotation_text = None, showlegend = True)
+    fig.add_hline(y = average, line_dash = 'solid', line_color = 'yellow', name = 'Average Volume' , showlegend = True)
+    fig.add_hline(y = average_period, line_dash = 'dash', line_color = avg_line_color, name = 'Average Volume last 3 Months', showlegend = True)
     fig.update_xaxes(title = 'Price')
     fig.update_layout(
     dragmode = False,
