1f658346556ca5ec56c36e0357f1a5f3a8825f3d
Thien Nhan Tran <127055768+JaydenT107@users.noreply.github.com>
2025-01-25 21:08:38 -0600

    a

--- Streamlit_script.py
+++ Streamlit_script.py
@@ -131,7 +131,7 @@ def line_chart(data,name,sday,eday,date_format,new_title=None, add_trendline = F
         line_end_date = filtered_data_2.iloc[-1]['Date']
         line_end_value = filtered_data_2.iloc[-1]['Close']
 
-        fig.add_shape(type = 'line', x0 = line_start_date, y0 = line_start_value, x1 = line_end_date, y1 = line_start_value, line = dict(color = line_color_2, width = 2))
+        fig.add_shape(type = 'line', x0 = line_start_date, y0 = line_start_value, x1 = line_end_date, y1 = line_end_value, line = dict(color = line_color_2, width = 2))
 
     
 
