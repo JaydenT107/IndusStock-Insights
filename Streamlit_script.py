e0ce56847205ab5f5cc2b32998a516192eefbd7e
Thien Nhan Tran <127055768+JaydenT107@users.noreply.github.com>
2025-01-25 23:25:35 -0600

    a

--- Streamlit_script.py
+++ Streamlit_script.py
@@ -132,8 +132,8 @@ def line_chart(data,name,sday,eday,date_format,new_title=None, add_trendline = F
         line_end_value = filtered_data_2.iloc[-1]['Close']
 
         fig.add_shape(type = 'line', x0 = line_start_date, y0 = line_start_value, x1 = line_end_date, y1 = line_end_value, line = dict(color = line_color_2, width = 2, dash = 'dash'))
-        fig.add_annotation(y = line_start_value, x = line_start_date, showarrow = True, text = f"{line_start_value}", ax = -50 , ay = -30, borderwidth = 0.1)
-        fig.add_annotation(y = line_end_value, x = line_end_date, showarrow = True, text = f"{line_end_value}", ax = -50 , ay = 30, borderwidth = 0.1, bgcolor = 'yellow')
+        fig.add_annotation(y = line_start_value, x = line_start_date, showarrow = True, text = f"{line_start_value}", ax = -50 , ay = -30, borderwidth = 0.1, arrowcolor = 'yellow')
+        fig.add_annotation(y = line_end_value, x = line_end_date, showarrow = True, text = f"{line_end_value}", ax = -50 , ay = 30, borderwidth = 0.1, arrowcolor = 'yellow')
     
 
     return [fig,line_color]
