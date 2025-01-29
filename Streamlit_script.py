e1ea62d60df844bcacd78b022aa5facb2ef3ba02
Thien Nhan Tran <127055768+JaydenT107@users.noreply.github.com>
2025-01-28 23:38:49 -0600

    a

--- Streamlit_script.py
+++ Streamlit_script.py
@@ -309,8 +309,8 @@ def second_part(s3 = s3client):
         st.plotly_chart(scatter_plot(data,sday = sday,eday = eday, date_format = date_format, new_title = None , name = None, date_format_2 = date_format_2), use_container_width = True, config = {'displayModeBar' : False})
 
     with col2:
-        st.markdown(f"<h1 style='font-size: 200px; color: black ;'> | </h1>", unsafe_allow_html=True)
-        st.markdown(f"<h1 style='font-size: 200px; color: black ;'> | <span style='color: black;'> </span></h1>", unsafe_allow_html=True)
-        st.markdown(f"<h1 style='font-size: 200px; color: black ;'> | <span style='color: black;'> </span></h1>", unsafe_allow_html=True)
+        st.markdown(f"<h1 style='font-size: 250px; color: black ;'> | </h1>", unsafe_allow_html=True)
+        st.markdown(f"<h1 style='font-size: 250px; color: black ;'> | <span style='color: black;'> </span></h1>", unsafe_allow_html=True)
+        st.markdown(f"<h1 style='font-size: 250px; color: black ;'> | <span style='color: black;'> </span></h1>", unsafe_allow_html=True)
         st.write(VAI_description)
 second_part()
