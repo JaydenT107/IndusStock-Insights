eb9c1c071960f18579082e8a09d0932b4c152257
Thien Nhan Tran <127055768+JaydenT107@users.noreply.github.com>
2025-01-30 00:35:10 -0600

    a

--- Streamlit_script.py
+++ Streamlit_script.py
@@ -316,20 +316,22 @@ def second_part(s3 = s3client, sector = sector):
                 word.insert(4,word[3].replace('-', ''))
                 word.remove(word[3])
                 word.insert(3, 'decrease by')
-                st.write(word)
-                f_close_description = " ".join(word)
-                return f_close_description
+                return word
             else:
                 word = close_description.split(" ")
                 word.insert(3, 'increase by')
-                f_close_description = " ".join(word)
-                return f_close_description
-
+                return word
+        f_close_description = cleaned_function(close_data)
         data = output_data[name.index(stock_name)] 
         st.markdown(f"<h1 style='font-size: 45px; color:#fffd7b ;'>{stock_name}</h1>", unsafe_allow_html=True)
         st.markdown(f"<h1 style='font-size: 30px; color: white;'>Period: <span style='color: yellow;'>{date_format.title()} vs. {date_format_2}</span></h1>", unsafe_allow_html=True)
         st.plotly_chart(line_chart(data,sday = sday,eday = eday, date_format = date_format, new_title = f'Price', name = None, add_trendline = True, date_format_2 = date_format_2)[0], use_container_width = True, config = {'displayModeBar' : False})
-        st.markdown(f"<h1 style='font-size: 20px; color:white ;'> {cleaned_function(close_description)} </h1>", unsafe_allow_html=True)
+       st.markdown(f"<h1 style='font-size: 20px; color:white;'>"
+            f"<span style='color: yellow;'>{f_close_description[0]}</span> "
+            f"{' '.join(f_close_description[1:3])} "
+            f"<span style='color: yellow;'>{f_close_description[3]} {f_close_description[4]} {f_close_description[5]}</span> "
+            f"{' '.join(f_close_description[6:])}</h1>", 
+            unsafe_allow_html=True)
         st.plotly_chart(volatility_chart(data,sday = sday,eday = eday, date_format = date_format, new_title = f'Price', name = None, date_format_2 = date_format_2), use_container_width = True, config = {'displayModeBar' : False})
         st.markdown(f"<h1 style='font-size: 20px; color:white ;'> {volume_description} </h1>", unsafe_allow_html=True)
     with col2:
