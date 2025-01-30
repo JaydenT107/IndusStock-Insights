b870dcaf61447f3a97173d48d6381347fd2c783e
Thien Nhan Tran <127055768+JaydenT107@users.noreply.github.com>
2025-01-29 19:59:58 -0600

    a

--- Streamlit_script.py
+++ Streamlit_script.py
@@ -317,7 +317,7 @@ def second_part(s3 = s3client):
         st.write(VAI_description)
     with col2:
         st.markdown(f"<h1 style='font-size: 100px; color:black ;'>|</h1>", unsafe_allow_html=True)
-        st.markdown(f"<h1 style='font-size: 100px; color:black ;'>|</h1>", unsafe_allow_html=True)
+        st.markdown(f"<h1 style='font-size: 110px; color:black ;'>|</h1>", unsafe_allow_html=True)
         st.plotly_chart(scatter_plot(data,sday = sday,eday = eday, date_format = date_format, new_title = None , name = None, date_format_2 = date_format_2), use_container_width = True, config = {'displayModeBar' : False})
         st.write(VoAI_description)
 second_part()
