02df2c586c05bf7e52b8f86eb1f532fd9fc56053
Thien Nhan Tran <127055768+JaydenT107@users.noreply.github.com>
2025-01-29 21:34:37 -0600

    a

--- Streamlit_script.py
+++ Streamlit_script.py
@@ -301,9 +301,9 @@ def second_part(s3 = s3client, sector = sector):
         close_description = close_data[f'{stock_name}'][cdate_list.index(date_format_2)]
         
         if '-' in close_description.split():
-            close_description = close_description[10:12] + 'decrease by' + close_description[2::]
+            close_description = close_description[0:13] + 'decrease by' + close_description[2::]
         else:
-            close_description = close_description[10:2] + 'increase by' + close_description[2::]
+            close_description = close_description[0:13] + 'increase by' + close_description[2::]
 
         st.write(close_description)
 
