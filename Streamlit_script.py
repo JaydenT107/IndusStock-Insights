eee5553b871c42171d44aa084b1bbe1012363d8d
Thien Nhan Tran <127055768+JaydenT107@users.noreply.github.com>
2025-01-29 21:45:55 -0600

    a

--- Streamlit_script.py
+++ Streamlit_script.py
@@ -302,10 +302,13 @@ def second_part(s3 = s3client, sector = sector):
         
         if '-' in list(close_description):
             word = close_description.split(" ")
-            word.insert(2, 'decrease by')
+            word[4].replace('-', '')
+            word.insert(3, 'decrease by')
             f_close_description = " ".join(word)
         else:
-            close_description = close_description[0:16] + 'increase by ' + close_description[16::]
+            word = close_description.split(" ")
+            word.insert(3, 'increase by')
+            f_close_description = " ".join(word)
 
         st.write(f_close_description)
 
