9e4c02d8604db51a844f228ebc2575b952045dc9
Thien Nhan Tran <127055768+JaydenT107@users.noreply.github.com>
2025-01-29 20:34:45 -0600

    a

--- Streamlit_script.py
+++ Streamlit_script.py
@@ -278,7 +278,8 @@ def first_part():
     return name,data,sday,eday,date_format 
 
 def second_part(s3 = s3client):
-    
+    name,output_data,sday,eday,date_format  = first_part()
+
     
     st.header('Explore Stock Details')
     col1,col2 = st.columns(2)
@@ -298,10 +299,12 @@ def second_part(s3 = s3client):
         st.markdown(f"<h1 style='font-size: 45px; color:#fffd7b ;'>{stock_name}</h1>", unsafe_allow_html=True)
         st.markdown(f"<h1 style='font-size: 30px; color: white;'>Period: <span style='color: yellow;'>{date_format.title()} vs. {date_format_2}</span></h1>", unsafe_allow_html=True)
         st.plotly_chart(line_chart(data,sday = sday,eday = eday, date_format = date_format, new_title = f'Price', name = None, add_trendline = True, date_format_2 = date_format_2)[0], use_container_width = True, config = {'displayModeBar' : False})
+       
         st.plotly_chart(volatility_chart(data,sday = sday,eday = eday, date_format = date_format, new_title = f'Price', name = None, date_format_2 = date_format_2), use_container_width = True, config = {'displayModeBar' : False})
+        
     with col2:
         st.markdown(f"<h1 style='font-size: 100px; color:black ;'>|</h1>", unsafe_allow_html=True)
         st.markdown(f"<h1 style='font-size: 110px; color:black ;'>|</h1>", unsafe_allow_html=True)
         st.plotly_chart(scatter_plot(data,sday = sday,eday = eday, date_format = date_format, new_title = None , name = None, date_format_2 = date_format_2), use_container_width = True, config = {'displayModeBar' : False})
-        st.write(VoAI_description)
+       
 second_part()
