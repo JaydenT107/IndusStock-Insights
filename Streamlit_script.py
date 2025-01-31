55e54cadc888382e760d064397e4ca426dcb1695
Thien Nhan Tran <127055768+JaydenT107@users.noreply.github.com>
2025-01-30 20:36:06 -0600

    a

--- Streamlit_script.py
+++ Streamlit_script.py
@@ -202,7 +202,7 @@ def scatter_plot(data,name,sday,eday,date_format, date_format_2,new_title=None):
     title='Correlation Between Trading Volume and Closing Price',            
     title_font=dict(size=24, family='Soin Sans Pro', color='white'),
     width = 600,
-    height = 400,
+    height = 350,
      legend=dict( x=0,  y=0.98, title = None, borderwidth = 0.3, font = dict(size = 12),bgcolor="rgba(0,0,0,0)")
 )
 
@@ -355,7 +355,6 @@ def second_part(s3 = s3client, sector = sector):
         st.markdown(f"<h1 style='font-size: 150px; color:black ;'>|</h1>", unsafe_allow_html=True)
         st.markdown(f"<h1 style='font-size: 115px; color:black ;'>|</h1>", unsafe_allow_html=True)
         st.plotly_chart(scatter_plot(data,sday = sday,eday = eday, date_format = date_format, new_title = None , name = None, date_format_2 = date_format_2), use_container_width = True, config = {'displayModeBar' : False})
-        st.markdown(f"<h1 style='font-size: 1px; color:black ;'>|</h1>", unsafe_allow_html=True)
         st.markdown(f"<h1 style='font-size: 20px; color:white;'>"
             f"<span style='color: yellow;'>{volume_description.split(" ")[0]}</span> "
             f"stock average volatility over the past month: "
