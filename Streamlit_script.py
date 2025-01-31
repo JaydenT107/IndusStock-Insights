539e0ac03a3663cc0c1af4f8bd2ad90202f6d9fe
Thien Nhan Tran <127055768+JaydenT107@users.noreply.github.com>
2025-01-30 21:46:13 -0600

    a

--- Streamlit_script.py
+++ Streamlit_script.py
@@ -254,7 +254,7 @@ def volatility_chart(data,name,sday,eday,date_format, date_format_2,new_title=No
 
     return fig
 
-
+def recommendation_chart(data)
 
 def date_format_func(data):
     if data == '3 Months' or data == '6 Months' or data == '1 Year':
@@ -374,5 +374,5 @@ def second_part(s3 = s3client, sector = sector):
             f"stock average volatility over the past {date_format_2.lower()}: "
             f"<span style='color: yellow;'>{volume_description.split(" ")[-1]}</span></h1>", 
             unsafe_allow_html=True)
-        st.write(recommendation_tables)
+        st.write(recommendation_tables[name.index(stock_name)])
 second_part()
