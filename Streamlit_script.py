7496e8e17d81d21c7ed65b3fba0cbc994e78da2d
Thien Nhan Tran <127055768+JaydenT107@users.noreply.github.com>
2025-01-30 22:59:05 -0600

    a

--- Streamlit_script.py
+++ Streamlit_script.py
@@ -103,8 +103,8 @@ def get_data(sday = gsday, eday = geday, period_index = period_index, date_forma
 
         recommendation = recommendation_response['Body'].read().decode('utf-8')
         rdf = pd.read_csv(StringIO(recommendation))
-        rdf.rename(columns = {'StrongBuy' : 'Strong Buy'}, inplace = True)
-        rdf.rename(columns = {'StrongSell' : 'Strong Sell'}, inplace = True)
+        rdf.rename(columns = {'strongBuy' : 'Strong Buy'}, inplace = True)
+        rdf.rename(columns = {'strongSell' : 'Strong Sell'}, inplace = True)
         recommendation_tables.append(rdf)
 
     return [tables,names_list,sector, Display_AI_description, sday,eday, date_format,recommendation_tables]
