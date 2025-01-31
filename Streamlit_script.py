c0b8a439e121642bb6f99a96232f4fd0b1678c13
Thien Nhan Tran <127055768+JaydenT107@users.noreply.github.com>
2025-01-30 18:39:53 -0600

    a

--- Streamlit_script.py
+++ Streamlit_script.py
@@ -160,9 +160,9 @@ def line_chart(data,name,sday,eday,date_format, date_format_2 ,new_title=None, a
         line_end_value = filtered_data_2.iloc[-1]['Close']
         random_ax1 = random.randint(-50,50)
         random_ax2 = random.randint(-50,50)
-        fig2.add_shape(type = 'line', x0 = line_start_date, y0 = line_start_value, x1 = line_end_date, y1 = line_end_value, line = dict(color = line_color_2, width = 2, dash = 'dash'))
-        fig2.add_shape(type = 'line', x0 = line_start_date, y0 = line_start_high, x1 = line_end_date, y1 = line_end_high, line = dict(color = line_color_2, width = 2, dash = 'dash'))
-        fig2.add_shape(type = 'line', x0 = line_start_date, y0 = line_start_low, x1 = line_end_date, y1 = line_end_low, line = dict(color = line_color_2, width = 2, dash = 'dash'))
+        fig2.add_shape(type = 'line', x0 = line_start_date, y0 = line_start_value, x1 = line_end_date, y1 = line_end_value, line = dict(color = line_color_2, width = 2, dash = 'line'))
+        fig2.add_shape(type = 'line', x0 = line_start_date, y0 = line_start_high, x1 = line_end_date, y1 = line_end_high, line = dict(color = 'yellow', width = 2, dash = 'dash'))
+        fig2.add_shape(type = 'line', x0 = line_start_date, y0 = line_start_low, x1 = line_end_date, y1 = line_end_low, line = dict(color = 'yellow', width = 2, dash = 'dash'))
         fig2.add_annotation(y = line_start_value, x = line_start_date, showarrow = True, text = f"{line_start_value}", ax = random_ax1 , ay = random_ax2, borderwidth = 0.1, arrowcolor = 'yellow')
         fig2.add_annotation(y = line_end_value, x = line_end_date, showarrow = True, text = f"{line_end_value}", ax = random_ax2 , ay = random_ax1, borderwidth = 0.1, arrowcolor = 'yellow')
         return [fig2,line_color]
